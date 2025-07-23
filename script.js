998483476f87f8a6a18179f6ac798da061a303b0
endingtelex177 <160832438+endingtelex177@users.noreply.github.com>
2025-07-23 20:56:57 +0800

    added html and script

---
+++ script.js
@@ -0,0 +1,20 @@
+//h1 element of the timer 
+
+//the timer identity
+let timer_display = document.getElementById("timer");
+
+//a function that let user input their own interval timer and the timer will start with a press of a button
+
+// setInterval(() => {
+//    timer_display.innerHTML = t 
+//    t--
+// },1000)
+
+// taken a input from the input id 
+const timer_button = document.getElementById("button_timer")
+
+timer_button.onclick = ()=>{
+    let user_value = document.getElementById("input_timer").value;
+    timer_display.innerHTML = user_value;
+}
+
