d3e8b9f441a6c432ee9c3549756bcfc57cafe464
endingtelex177 <amirmirza2503@gmail.com>
2025-07-24 11:01:07 +0800

    added in style.css, added in placeholder for input type

--- script.js
+++ script.js
@@ -13,8 +13,11 @@ let timer_display = document.getElementById("timer");
 // taken a input from the input id 
 const timer_button = document.getElementById("button_timer")
 
-timer_button.onclick = ()=>{
+timer_button.onclick = ()=> {
     let user_value = document.getElementById("input_timer").value;
     timer_display.innerHTML = user_value;
 }
+
+//~~~~~ using padStart to add '0' on the number. have to be converted into string first ~~~~~
+// console.log("3".padStart(2,0))
 
